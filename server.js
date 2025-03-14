aacdc787f1e7d058b0bef71bbd5a783f506499cd
jeffrey-thomas <jeffrey.craig.thomas@gmail.com>
2025-03-14 14:03:09 -0700

    add express server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import { router } from './routes/index.js';
 
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/',()=>{
-    
-})
+app.use('/',router)
+
+app.listen(port, ()=>{console.log(`Listening on port ${3000}`)});
